bcc8227cd730ab5a6fd92b66c4d6e8e089916843
Harlod Bombala <h.bombala@student.avans.nl>
2024-03-22 17:22:18 +0100

    installed full calendar and created calender view

--- resources/views/Rental/rental-overview.blade.php
+++ resources/views/Rental/rental-overview.blade.php
@@ -1,7 +1,31 @@
+@push('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js"></script>
+@endpush
+<link href="https://cdn.jsdelivr.net/npm/@fullcalendar/core/main.css" rel="stylesheet" />
+<link href="https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid/main.css" rel="stylesheet" />
+<link href="https://cdn.jsdelivr.net/npm/@fullcalendar/timegrid/main.css" rel="stylesheet" />
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Rentals') }}
         </h2>
     </x-slot>
+
+    <div id="calendar" class="bg-white p-4 shadow-md rounded-md"></div>
+
+    @push('scripts')
+        <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.js"></script>
+        <script>
+            document.addEventListener('DOMContentLoaded', function () {
+                var calendarEl = document.getElementById('calendar');
+                var calendar = new FullCalendar.Calendar(calendarEl, {
+                    initialView: 'timeGridWeek',
+                    slotMinTime: '8:00:00',
+                    slotMaxTime: '19:00:00',
+                    {{--events: @json($events),--}}
+                });
+                calendar.render();
+            });
+        </script>
+    @endpush
 </x-app-layout>
